f7d39c9e9481a46df01d18b9ce92362dec371136
Jérémy <>
2017-11-03 16:07:49 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="fr" >
+<head>
+    <title>OGP - Outils de Gestion Personnels</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <script src="https://use.fontawesome.com/c15d6c3134.js"></script>
+    <script
+        src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous"></script>
+    <script src="semantic/dist/semantic.min.js"></script>
+</head>
+
+<body>
+    <h1><?php echo 'It works!'; ?></h1>
+    <div class="ui primary button">
+        <i class="shop icon"></i> Add to Cart
+    </div>
+    <div class="ui secondary button">
+        Cancel
+    </div>
+    <div class="ui card">
+        <div class="image">
+            <img src="https://semantic-ui.com/images/avatar/large/jenny.jpg">
+        </div>
+        <div class="content">
+            <a class="header">Regard 1</a>
+            <div class="meta">
+                <span class="date">35x35</span>
+            </div>
+        </div>
+        <div class="extra content">
+            <span class="right floated">
+                Novembre 2017
+            </span>
+            <i class="fa fa-eur" aria-hidden="true"></i> 575
+        </div>
+    </div>
+</body>
+</html>
